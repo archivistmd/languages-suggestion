fbff6c85f5273043bf816ba2a6583bf148acfe39
Mesha <madsketchz@gmail.com>
2023-01-13 12:12:45 -0800

    Add final responses, js update

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
-window.onload = function() {
-  // the form element
-  const form = document.querySelector("form");
-  // event handler for the form's submission
-  form.onsubmit = function(event) {
-    event.preventDefault();
-
-    document.querySelector("span#name") .innertext = text;
-
-    function handleSelect(event) {
-      event.preventDefault();
-      const selection = document.getElementById("baseQs").value;
-    }
-    
-    window.addEventListener("load", function() {
-      document.getElementById("select-form").addEventListener("submit", handleSelect);
-    });
-  }
+function languageSuggestion(event) {
+  event.preventDefault();
 }
+
+    const pickProgram = document.querySelector("input[name='pickProgramA']:checked")
+    const motivation = document.querySelector("input[name='motivation']:checked")
+    const comfy = document.querySelector("input[name='comfy']:checked")
+    const game = document.querySelector("input[name='game']:checked")
+
+    if (pickProgram === "apps" && motivation == "back") {
+      document.querySelector("div.ruby").removeAttribute(".hidden")
+    } else if (comfy === "youtube") {
+      document.querySelector("div#python").removeAttribute(".hidden")
+    } else {
+      document.querySelector("div.ruby").removeAttribute(".hidden")
+    };
+
+    if (pickProgram === "games" && motivation == "back") {
+    document.querySelector("div.python").removeAttribute(".hidden")
+    } else if (comfy === "wait") {
+    document.querySelector("div#explore").removeAttribute(".hidden")
+    };
+
+    if (pickProgramA === "web" && motivation == "back") {
+    document.querySelector("div.ruby").removeAttribute(".hidden")
+    } else if (comfy === "tumblr") {
+    document.querySelector("div#js").removeAttribute(".hidden")
+    };
+    
+    if (pickProgramA === "dogs" && motivation == "dog") {
+    document.querySelector("div.explore").removeAttribute(".hidden")
+    } else if (comfy === "hacker") {
+    document.querySelector("div#js").removeAttribute(".hidden")
+    };
+  
+  window.addEventListener("load", function(){
+    const form = this.document.getElementByID("form");
+    form.addEventListener("submit", languageSuggestion)
+  })
