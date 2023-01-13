f919aa1058a8f67d374a2a9e595b5c269ed9bd61
Mesha <madsketchz@gmail.com>
2023-01-13 10:19:14 -0800

    Add background colour, font style, js form

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ window.onload = function() {
   // event handler for the form's submission
   form.onsubmit = function(event) {
     event.preventDefault();
+
+    function handleSelect(event) {
+      event.preventDefault();
+      const selection = document.getElementById("baseQs").value;
+    }
+    
+    window.addEventListener("load", function() {
+      document.getElementById("select-form").addEventListener("submit", handleSelect);
+    });
