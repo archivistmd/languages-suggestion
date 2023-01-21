8ae30e4924c2474a1c3fa8948687fd9228220d38
Mesha <madsketchz@gmail.com>
2023-01-20 18:35:48 -0800

    WIP attempting to get results to show. Adjust to check specific radio button.

--- js/scripts.js
+++ js/scripts.js
@@ -12,56 +12,45 @@ function hideResults() {
       event.preventDefault();
       hideResults();
 
-    const pickProgram = document.querySelector("pickProgram").value;
-    const motivation = document.querySelector("motivation").value;
-    const comfy = document.querySelector("comfy").value;
+    const pickProgram = document.querySelector("input[name='comfy']:checked");
+    const motivation = document.querySelector("input[name='motivation']:checked");
+    const comfy = document.querySelector("input[name='comfy']:checked");
 
     const results = pickProgram + motivation + comfy;
 
-    if (pickProgram === "apps" && motivation === "back" && comfy === "tumblr") {
-      document.querySelector("js").removeAttribute(".hidden")
-    } else if (pickProgram === "apps" && motivation === "back" && comfy === ".hacker") {
+    if ((pickProgram === "apps" || pickProgram === "web") && motivation === "front" && comfy === "tumblr") {
+      document.querySelector("js").removeAttribute(".hidden");
+    } else if ((pickProgram ==="apps" || pickProgram === "web") && motivation === "front" && comfy === ".youtube") {
+      document.querySelector("ruby").removAttribute(".hidden");
+    } else if ((pickProgram === "apps" || pickProgram === "games") && motivation === "back" && comfy === ".hacker") {
       document.querySelector("python").removeAttribute(".hidden")
-    } else if (pickProgram === "apps" && motivation === "front" && comfy === ".youtube") {
-      document.querySelector("ruby").removeAttribute(".hidden")
-    } else if (pickProgram === "apps" && motivation === "wait" && comfy === ".hacker"){
-      document.querySelector("explore").removeAttribute(".hidden")
-    };
-
-    if (pickProgram === "games" && motivation === "back" && comfy === "hacker") {
-      document.querySelector("python").removeAttribute(".hidden")
-    } else if (pickProgram === "game" && motivation === "dog" && comfy === "youtube") {
-      document.querySelector("explore").removeAttribute(".hidden")
-    } else if (pickProgram === "game" && motivation === "front" && comfy === "youtube") {
-      document.querySelector("ruby").removeAttribute(".hidden")
-    } else if (pickProgram === "game" && motivation === "back" && comfy === "youtube") {
-      document.querySelector("js").removeAttribute(".hidden")
-    } else if (pickProgram === "game" && motivation === "dog" && comfy === "hacker") {
-      document.querySelector("explore").removeAttribute(".hidden")
-    };
-
-    if (pickProgram === "web" && motivation === "back" && comfy === "youtube") {
-      document.querySelector("ruby").removeAttribute(".hidden")
+    } else if ((pickProgram === "apps" || pickProgram === "web") && motivation === "wait" && comfy === ".hacker"){
+      document.querySelector("explore").removeAttribute(".hidden");
+    } else if ((pickProgram === "game" || pickProgram === "web") && motivation === "dog" && comfy === "youtube") {
+      document.querySelector("explore").removeAttribute(".hidden");
+    } else if ((pickProgram === "game" || pickProgram === "web") && motivation === "front" && comfy === "youtube") {
+      document.querySelector("ruby").removeAttribute(".hidden");
+    } else if ((pickProgram === "game" || pickProgram === "web") && motivation === "dog" && comfy === "hacker") {
+      document.querySelector("explore").removeAttribute(".hidden");
+    } else if (pickProgram === "web" && motivation === "back" && comfy === "youtube") {
+      document.querySelector("ruby").removeAttribute(".hidden");
     } else if (pickProgram === "web" && motivation === "front" && comfy === "youtube") {
-      document.querySelector("js").removeAttribute(".hidden")
+      document.querySelector("js").removeAttribute(".hidden");
     } else if (pickProgram === "web" && motivation === "wait" && comfy === "hacker") {
-      document.querySelector("explore").removeAttribute(".hidden")
+      document.querySelector("explore").removeAttribute(".hidden");
     } else if (pickProgram === "web" && motivation === "dog" && comfy === "tumblr") {
-      document.querySelector("js").removeAttribute(".hidden")
+      document.querySelector("js").removeAttribute(".hidden");
     } else if (pickProgram === "web" && motivation === "dog" && comfy === "hacker") {
-      document.querySelector("explore").removeAttribute(".hidden")
-    };
-    
-    if (pickProgram === "dogs" && motivation === "dog" && comfy === "hacker") {
-      document.querySelector("explore").removeAttribute(".hidden")
+      document.querySelector("explore").removeAttribute(".hidden");    
+    } else if (pickProgram === "dogs" && motivation === "dog" && comfy === "hacker") {
+      document.querySelector("explore").removeAttribute(".hidden");
     } else if (pickProgram === "dogs" && motivation === "front" && comfy === "tumblr") {
-      document.querySelector("js").removeAttribute(".hidden")
+      document.querySelector("js").removeAttribute(".hidden");
     } else if (pickProgram === "dogs" && motivation === "front" && comfy === "youtube") {
-      document.querySelector("ruby").removeAttribute(".hidden")
-    } else if (pickProgram === "dogs" && motivation === "back" && comfy === "youtube") {
-      document.querySelector("python").removeAttribute(".hidden")
-    } else if (pickProgram === "dogs" && motivation === "dog" && comfy === "wait") {
+      document.querySelector("ruby").removeAttribute(".hidden");
+    } else if (9pickProgram === "dogs" || pickProgram === "games") && motivation === "back" && comfy === "hacker") {
+      document.querySelector("python").removeAttribute(".hidden");
+    } else if ((pickProgram === "dogs" || pickProgram === "apps") && motivation === "dog" && comfy === "wait") {
       document.querySelector("explore").removeAttribute(".hidden")
     };
-    });
 });
